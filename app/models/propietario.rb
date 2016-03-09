32a671bd4b7102a1854e6e47a4900afa212ed182
Vitaly Shlyaga <storkvist@storkvist.net>
2016-03-10 00:26:11 +0300

    Added form for adding property by user
    User record is created automatically when property is added.

--- app/models/propietario.rb
+++ app/models/propietario.rb
@@ -1,12 +1,20 @@
 class Propietario < ActiveRecord::Base
 	belongs_to :admin
 
+	has_many :propiedads
+
 	def nombre_completo
   	"#{self.nombre} #{self.apellido}"
   end
 
-	validates :nombre, :apellido, :celular, :admin_id, presence: true
+	validates :nombre, :apellido, :celular, presence: true
 	validates :email, email: true
+
+  # This validation should use some sort of propietario_status. Because when
+  # user just register on site — he has no admin_id. I think admin_id should be
+  # filled in afterwards by admins.
+  # Vitaly
+  # validates_presence_of :admin
 
 	rails_admin do
 		object_label_method :nombre_completo
