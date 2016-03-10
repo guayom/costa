713250acb3f41a39bf9b1bb8e9e647bd7e930fac
Guayo Mena <guayo.mena@gmail.com>
2016-03-10 09:47:03 -0600

    Added new admins to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,23 @@ Admin.create!([
   {:nombre => 'Pablo Larios Navarro', :password => '12345678', :password_confirmation => '12345678', :codigo => 'P', :telefono => '8380-0415', :email => 'pablo@costa506realestate.com'},
   {:nombre => 'Ivannia Contreras', :password => '12345678', :password_confirmation => '12345678', :codigo => 'I', :telefono => '8380-0415', :email => 'pablo@costa506realestate.com'},
   {:nombre => 'Christian Ungar Borbon', :password => '12345678', :password_confirmation => '12345678', :codigo => 'C', :telefono => '6006-0502', :email => 'christian@costa506realestate.com'},
+  {:nombre => 'Sebastián Aguilar Barquero', :password => '12345678', :password_confirmation => '12345678', :codigo => 'S', :telefono => '8882-6473', :email => 'sebastian@costa506realestate.com'},
+  {:nombre => 'Usuario no identificado', :password => '12345678', :password_confirmation => '12345678', :codigo => 'M', :telefono => '8882-6473', :email => 'm@guayo.me'}
 ])
 
 Caracteristica.delete_all
 Caracteristica.create!([
-	{}
+	{:titulo => 'piscina', :type => 'amenidad'},
+	{:titulo => 'generador', :type => 'otros'},
+	{:titulo => 'ascensor', :type => 'otros'},
+	{:titulo => 'gimnasio', :type => 'otros'},
+	{:titulo => 'oficina', :type => 'otros'},
+	{:titulo => 'internet', :type => 'otros'},
+	{:titulo => 'seguridad', :type => 'otros'},
+	{:titulo => 'lavandería', :type => 'otros'},
+	{:titulo => 'vista_al_mar', :type => 'otros'},
+	{:titulo => 'chimenea', :type => 'otros'},
+	{:titulo => 'sala_de_reuniones', :type => 'otros'},
+	{:titulo => 'parque', :type => 'otros'},
+	{:titulo => 'ascensor', :type => 'otros'},
 ])
