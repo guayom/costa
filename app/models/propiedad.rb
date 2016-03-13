6a8ae36bd5f9f1ebec1dfe8e58a41dc596925db2
Vitaly Shlyaga <storkvist@storkvist.net>
2016-03-13 12:35:59 +0300

    Added characteristics list to propiedades#show

--- app/models/propiedad.rb
+++ app/models/propiedad.rb
@@ -15,7 +15,6 @@ class Propiedad < ActiveRecord::Base
   has_and_belongs_to_many :mensajes
   has_and_belongs_to_many :caracteristicas
   # accepts_nested_attributes_for :imagenes, :allow_destroy => true
-  #has_and_belongs_to_many :caracteristicas
 
   has_and_belongs_to_many :tipos, dependent: :destroy
   # accepts_nested_attributes_for :tipos
