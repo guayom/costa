670d9744df1a8ce2db030a1caac18936158a3ec5
Vitaly Shlyaga <storkvist@storkvist.net>
2016-03-17 14:53:46 +0300

    Fix

--- db/migrate/20160317114626_remove_monetize_fields_from_propiedades.rb
+++
@@ -1,7 +0,0 @@
-class RemoveMonetizeFieldsFromPropiedades < ActiveRecord::Migration
-  def change
-    remove_columns :propiedades,
-                   :valor_compra_cents, :valor_compra_currency,
-                   :valor_alquiler_cents, :valor_alquiler_currency
-  end
-end
