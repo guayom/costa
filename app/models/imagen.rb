51e22816fe708e4605d1f5c65f98855c57f11d0f
Vitaly Shlyaga <storkvist@storkvist.net>
2016-04-01 11:10:28 +0300

    Added watermark processor to Large thumbnail of property image.

--- app/models/imagen.rb
+++ app/models/imagen.rb
@@ -4,18 +4,21 @@ class Imagen < ActiveRecord::Base
   belongs_to :propiedad, inverse_of: :imagenes
 
   has_attached_file :imagen,
-    :path  => "/images/:id/:style/:filename",
+    :path  => '/images/:id/:style/:filename',
     :styles => {
-      :thumb => "100x65>",
-      :small  => "260x173>",
-      :medium => "358x238>",
-      :large => "786x522>" 
-    }
+      :thumb => '100x65>',
+      :small  => '260x173>',
+      :medium => '358x238>',
+      large: {
+        geometry: '786x522>',
+        watermark_path: Rails.root.join('app', 'assets', 'images', 'watermark-white.png')
+      }
+    },
+    processors: [:watermark]
 
   def imagen_from_url(url)
     self.imagen = open(url)
   end
 
   validates_attachment_content_type :imagen, :content_type => /\Aimage\/.*\Z/
-
 end
--- lib/paperclip/watermark.rb
+++ lib/paperclip/watermark.rb
@@ -3,11 +3,42 @@ module Paperclip
     def initialize(file, options = {}, attachment = nil)
       super
       @watermark_path = options[:watermark_path]
-      @position = options[:position].present? ? options[:position] : 'SouthEast'
+      @position = options[:position].present? ? options[:position] : 'south'
+      @dissolve = options[:dissolve].present? ? options[:dissolve] : '25%'
+    end
+
+    def composite(arguments = '', local_options = {})
+      Paperclip.run('composite', arguments, local_options)
     end
 
     def make
+      src = super
 
+      if @watermark_path
+        filename = [@basename, @format ? ".#{@format}" : ''].join
+        dst = TempfileFactory.new.generate(filename)
+
+        begin
+          parameters = []
+          parameters << "-dissolve #{@dissolve}"
+          parameters << "-gravity #{@position}"
+          parameters << "'#{@watermark_path}'"
+          parameters << src.path
+          parameters << ':dest'
+
+          parameters = parameters.flatten.compact.join(' ').strip.squeeze(' ')
+
+          composite(parameters, source: File.expand_path(src.path), dest: File.expand_path(dst.path))
+        rescue Cocaine::ExitStatusError
+          raise Paperclip::Error, "There was an error adding the watermark to #{@basename}" if @whiny
+        rescue Cocaine::CommandNotFoundError
+          raise Paperclip::Errors::CommandNotFoundError.new('Could not run the `composite` command. Please install ImageMagick.')
+        end
+
+        dst
+      else
+        src
+      end
     end
   end
 end
