16d688ac864b1be765e5683ba33f4fedd76e26fa
Vitaly Shlyaga <storkvist@storkvist.net>
2016-04-03 13:01:09 +0300

    Changed view and position of watermark on large property thumbnail.

--- lib/paperclip/watermark.rb
+++ lib/paperclip/watermark.rb
@@ -3,8 +3,12 @@ module Paperclip
     def initialize(file, options = {}, attachment = nil)
       super
       @watermark_path = options[:watermark_path]
-      @position = options[:position].present? ? options[:position] : 'south'
-      @dissolve = options[:dissolve].present? ? options[:dissolve] : '25%'
+      @position = options[:position].present? ? options[:position] : 'SouthEast'
+      @dissolve = options[:dissolve].present? ? options[:dissolve] : '95%'
+    end
+
+    def identify(arguments = '', local_options = {})
+      Paperclip.run('identify', arguments, local_options)
     end
 
     def composite(arguments = '', local_options = {})
@@ -19,9 +23,20 @@ module Paperclip
         dst = TempfileFactory.new.generate(filename)
 
         begin
+          src_size = identify("-format %wx%h #{src.path}").split('x').map(&:to_i)
+        rescue Cocaine::ExitStatusError => e
+          raise Paperclip::Error, "There was an error running `identify` for #{@basename}" if @whiny
+        rescue Cocaine::CommandNotFoundError => e
+          raise Paperclip::Errors::CommandNotFoundError.new("Could not run the `identify` command. Please install ImageMagick.")
+        end
+
+        begin
+          watermark_geometry = "#{(src_size[0] * 0.6).to_i}x#{src_size[1]}+30+25"
+
           parameters = []
           parameters << "-dissolve #{@dissolve}"
           parameters << "-gravity #{@position}"
+          parameters << "-geometry #{watermark_geometry}"
           parameters << "'#{@watermark_path}'"
           parameters << src.path
           parameters << ':dest'
