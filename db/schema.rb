226d0eef17b71f7cbcbf9cf9de8e95a8e4bc66ad
Guayo Mena <guayo.mena@gmail.com>
2016-05-05 07:13:03 -0600

    Fixed main logo and green line

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20160504092733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
