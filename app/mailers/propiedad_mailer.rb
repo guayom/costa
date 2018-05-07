a02c22f5e9d6340187d34b26364550f557914fd8
guayom <guayo.mena@gmail.com>
2018-05-06 20:43:50 -0600

    Use admin email from environement variable

--- app/mailers/propiedad_mailer.rb
+++ app/mailers/propiedad_mailer.rb
@@ -2,6 +2,6 @@ class PropiedadMailer < ApplicationMailer
   default from: "info@costa506realestate.com"
   def estado_email(propiedad)
     @propiedad = propiedad
-    mail(to:'guayo.mena@gmail.com', subject: "#{@propiedad.codigo} cambió a estado: #{@propiedad.estado}")
+    mail(to: ENV['ADMIN_EMAIL'], subject: "#{@propiedad.codigo} cambió a estado: #{@propiedad.estado}")
   end
 end
