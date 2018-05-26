8a81a5e69ac23d12d560a24381e1e28f303eada4
guayom <guayo.mena@gmail.com>
2018-05-26 11:48:29 -0600

    Remove letter opener module

--- config/application.rb
+++ config/application.rb
@@ -28,11 +28,3 @@ module Costa
     config.active_job.queue_adapter = :resque
   end
 end
-
-module LetterOpenerTest
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-  end
-end
