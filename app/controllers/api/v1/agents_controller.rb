331223fd10884d607a22357130d913dc89bc0e80
Guayo Mena <guayo.mena@gmail.com>
2019-05-22 15:35:42 -0600

    Make sure all agents are returned by api

--- app/controllers/api/v1/agents_controller.rb
+++ app/controllers/api/v1/agents_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class AgentsController < ApplicationController
       def index
-        @agents = Admin.all.where(permisos: "admin").order(:id)
+        @agents = Admin.all.where(permisos: ["admin", "agente"]).order(:id)
         respond_to :json
       end
     end
